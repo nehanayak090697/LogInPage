2f369eb16c587571bfc15df800d6da41587c7cc9
him123456789 <sabhimanyu809@gmail.com>
2021-08-21 14:59:54 +0530

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Auth from "./components/Auth"
+import Registration from "./components/Registration"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Link to="/Registration">Registration</Link><br />
+        <Link to="/">Login</Link>
+        <Switch>
+          <Route exact path="/" component={Auth} />
+          <Route path="/Registration" component={Registration} />
+        </Switch>
+      </Router>
+    </>
+
   );
 }
 
