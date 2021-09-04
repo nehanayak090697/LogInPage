8851199bf2dbf869937febc3ef10612d9ef3870e
nehanayak090697 <nehanayak090697@gmail.com>
2021-09-04 14:35:37 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 //import Auth from "./components/Auth"
 import Registration from "./components/Registration"
-import ContactUs from './components/ContactUs';
- import {
+import ContactUs from './components/Login';
+import {
  BrowserRouter as Router,
  Switch,
  Route,
